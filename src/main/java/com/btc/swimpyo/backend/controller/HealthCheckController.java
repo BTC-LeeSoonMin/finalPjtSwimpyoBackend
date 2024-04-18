7483587e0e53e60f627cfd83dd3c5a8ea75753b7
BTC-LeeSoonMin <soonmin07@gmail.com>
2024-04-18 21:41:15 +0900

    deploy: retry

--- src/main/java/com/btc/swimpyo/backend/controller/HealthCheckController.java
+++ src/main/java/com/btc/swimpyo/backend/controller/HealthCheckController.java
@@ -31,7 +31,6 @@ public class HealthCheckController {
         responseData.put("serverPort", serverPort);
         responseData.put("serverAddress", serverAddress);
         responseData.put("env", env);
-        responseData.put("name", "LSM");
 
         return ResponseEntity.ok(responseData);
     }
