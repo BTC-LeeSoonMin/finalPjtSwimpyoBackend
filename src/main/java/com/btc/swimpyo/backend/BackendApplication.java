71bd1c9565d8148a6f40a80744dde9ad81bf0624
BTC-LeeSoonMin <soonmin07@gmail.com>
2024-04-23 18:29:47 +0900

    depoly: react pjt deploy retry

--- src/main/java/com/btc/swimpyo/backend/BackendApplication.java
+++ src/main/java/com/btc/swimpyo/backend/BackendApplication.java
@@ -1,10 +1,9 @@
 package com.btc.swimpyo.backend;
 
-import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {MybatisAutoConfiguration.class})
+@SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
