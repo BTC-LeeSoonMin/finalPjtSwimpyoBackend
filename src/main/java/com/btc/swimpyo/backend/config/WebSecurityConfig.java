7958b4e022b34efeae62b64246d77e6c0f24cce0
BTC-LeeSoonMin <soonmin07@gmail.com>
2024-04-29 21:55:59 +0900

    Credential true [edit]

--- src/main/java/com/btc/swimpyo/backend/config/WebSecurityConfig.java
+++ src/main/java/com/btc/swimpyo/backend/config/WebSecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -59,8 +64,22 @@ public class WebSecurityConfig {
                 .requestMatchers("/api/user/**").hasRole("USER")
 //                .requestMatchers("/api/user/**").permitAll()
                 .anyRequest().authenticated()
+                .and()
+                .cors().configurationSource(corsConfigurationSource())
                 .and().build();
 
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true); // 이 부분을 추가합니다.
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
